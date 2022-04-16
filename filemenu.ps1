c9cf678520e5ebdcafcd856769f653bbe35082e5
Max Pereira <max@pereira.at>
2022-04-15 18:16:14 -0700

    Added filemenu

---
+++ filemenu.ps1
@@ -0,0 +1,11 @@
+﻿$files = Get-ChildItem -Path .\files
+$arrFiles = @(,@());
+foreach($file in $files ) {
+    $arrFile = $file
+    $arrFiles += ,$arrFile;
+}
+For ($i=1; $i -lt $arrFiles.Count ; $i++) {
+    Write-Host "$i : " $arrFiles[$i].BaseName
+}
+$chosenFile = Read-Host -Prompt "Choose a file"
+Start-Process ('.\files\' + $arrFiles[$chosenFile])
